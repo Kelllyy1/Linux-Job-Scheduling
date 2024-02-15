d88011e08afa79fcfe0cdcfd55de9e6df1e206b1
Kelllyy1 <65189136+Kelllyy1@users.noreply.github.com>
2024-02-15 13:24:12 -0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,4 +2,8 @@
 
 logfile=job_results.log
 
+#  Call the echo command to output this message in parenthesis into the "logfile"
 /usr/bin/echo "The script ran at the following time: $(/usr/bin/date)" > $logfile
+
+#  After the time specified when scheduling the job using the "at" command (refer to instructions.txt", enter the command
+  #  "cat job_results.log" to print the output to the terminal, and the message above along with the chosen time shouldbe displayed.
